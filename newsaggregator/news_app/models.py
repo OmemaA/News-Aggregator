ce8c6894cb78f5e056743a3427f771a38c05cdd9
Omema Ahmed <ahmed.omema@hotmail.com>
2022-04-22 20:44:16 +0500

    WIP: Part II

--- newsaggregator/news_app/models.py
+++ newsaggregator/news_app/models.py
@@ -1,8 +1,22 @@
 from django.db import models
+from numpy import source
 
-# Create your models here.
+# create your models here.
+
 class News(models.Model):
-    news_json = models.TextField()
-    source = models.CharField(max_length=100)
+    # search parameters for articles
+    parameters = models.TextField(primary_key=True)
+    # JSON object containing all articles for specified parameters
+    headline = models.TextField()
+    # date of fetch stored to calculate expiry date while searching
     saved_at_date = models.DateField()
-    parameters = models.TextField()
+    urls = models.TextField()
+    source = models.TextField()
+
+    # def __str__(self) -> str:
+    #     print(type(self.headline))
+    #     return str([self.headline, self.urls, self.source])
+
+    def getSavedDate(self):
+        return self.saved_at_date
+
--- newsaggregator/news_app/views.py
+++ newsaggregator/news_app/views.py
@@ -1,7 +1,10 @@
+from distutils.ccompiler import new_compiler
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
+from news_app.models import News
 import requests
-
+import praw
+import datetime
 
 # Create your views here.
 def index(req):
@@ -11,35 +14,86 @@ def index(req):
 
 def get_news(req):
     q = req.GET.get('q', '')
+
     API_KEY = '329db4fb718c407a8f394f328015ea57'
-    if q == '':
-        get_url = f'https://newsapi.org/v2/top-headlines?apiKey={API_KEY}&category=general&language=en'
-    else:
-        get_url = f'https://newsapi.org/v2/top-headlines?apiKey={API_KEY}&category=general&language=en&q={q}'
-
     
-    response = requests.get(get_url)
-    response = response.json()
-
-    #seperate articles from response
-    articles = response['articles']
+    reddit = praw.Reddit(
+    client_id= "JOMVkfinUF2r6p2niNo1fA",
+    client_secret= "JRUg34esT_xxDoXWnHLwEbVCCc9uDQ",
+    user_agent="win32:test_stellic_app:0.1.0 (by /u/Mindreader03")
+    # print(reddit.read_only)
 
     #create empty list to store headline, url and source
-    headlines = []
-    urls = []
-    sources = []
+    headlines, urls, sources = [], [], []
 
-    #loop through articles and append to empty list
-    for article in articles:
-        headlines.append(article['title'])
-        urls.append(article['url'])
-        sources.append(article['source']['name'])
-    
-    #create collection to store headlines, urls and sources
-    news_collection = zip(headlines, urls, sources)
+    if q == '':
+        news_collection = News.objects.filter(parameters='today')
+        print(news_collection.values())
+        if not news_collection.exists():
+            # for NewsAPI
+            get_url = f'https://newsapi.org/v2/top-headlines?apiKey={API_KEY}&category=general&language=en'
+            response = requests.get(get_url)
+            response = response.json()
+            # seperate articles from response
+            articles = response['articles']
+            # loop through articles and append to empty list
+            for article in articles:
+                headlines.append(article['title'])
+                urls.append(article['url'])
+                sources.append('newsapi')
+                # sources.append(article['source']['name'])
 
-    response = {
-        'news_collection': news_collection
-    }
+            # for Reddit
+            for article in reddit.subreddit('news').hot():
+                headlines.append(article.title)
+                urls.append(article.url)
+                sources.append('reddit')
+            # create collection to store headlines, urls and sources
+            news_collection = zip(headlines, urls, sources)
+            # save to database
+            headlines = str(headlines)
+            urls = str(urls)
+            sources = str(sources)
 
-    return render(req, 'index.html', response)
+            # articles = News(parameters='today', headline = str(headlines), urls = str(urls), source = str(sources), saved_at_date = datetime.date.today)
+            articles = News(parameters='today', headline = headlines, urls = urls, source = sources, saved_at_date = datetime.date.today)
+            articles.save()
+        else:
+            news_collection = list(news_collection)
+            print(news_collection)
+
+
+    else:
+        news_collection = News.objects.filter(parameters = q)
+        if not news_collection.exists():
+            # for NewsAPI
+            get_url = f'https://newsapi.org/v2/top-headlines?apiKey={API_KEY}&category=general&language=en&q={q}'
+            response = requests.get(get_url)
+            response = response.json()
+            # seperate articles from response
+            articles = response['articles']
+            # loop through articles and append to empty list
+            for article in articles:
+                headlines.append(article['title'])
+                urls.append(article['url'])
+                sources.append('newsapi')
+                # sources.append(article['source']['name'])
+            # for Reddit
+            all = reddit.subreddit("news")
+            for article in all.search(q):
+                headlines.append(article.title)
+                urls.append(article.url)
+                sources.append('reddit')
+            # create collection to store headlines, urls and sources
+            news_collection = zip(headlines, urls, sources)
+            # save to database
+            articles = News(parameters='today', headline = str(headlines), urls = str(urls), source = str(sources), saved_at_date = datetime.date.today)
+            articles.save()
+            
+    # response = {
+    #     'news_collection': news_collection
+    # }
+
+
+    # return render(req, 'index.html', response)
+    return HttpResponse("Hello, world. You're at the news index.")
